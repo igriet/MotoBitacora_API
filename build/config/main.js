ccf42c4b90ee04dfe9bf02541938c10cc0ed6607
16R137 <igriet.gonzalez@outlook.com>
2019-04-06 19:54:31 -0600

    Return everything to work local

--- build/config/main.js
+++ build/config/main.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var config = {
     port: process.env.PORT || 3000,
-    db: "mongodb://af3b888e-0ee0-4-231-b9ee:SikpPTUEgjnqMKduJ70F6mssmN2pLhN9P5aR7thuBf4VReEEuUKd6NGhOr9WFD3IcefbMJ0HlvieGxNnse1LCA%3D%3D@af3b888e-0ee0-4-231-b9ee.documents.azure.com:10255/?ssl=true",
+    db: 'mongodb://localhost/vitacoradb',
     test_env: "test",
-    test_db: "mongodb://af3b888e-0ee0-4-231-b9ee:SikpPTUEgjnqMKduJ70F6mssmN2pLhN9P5aR7thuBf4VReEEuUKd6NGhOr9WFD3IcefbMJ0HlvieGxNnse1LCA%3D%3D@af3b888e-0ee0-4-231-b9ee.documents.azure.com:10255/?ssl=true",
+    test_db: 'mongodb://localhost/vitacoradb',
     test_port: 3001
 };
 exports.default = config;
